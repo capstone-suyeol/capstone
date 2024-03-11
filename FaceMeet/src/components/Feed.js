9ec1581749284d40b5b0593192f0ac3f97456bab
juyoung <mywndud6@hanmail.net>
2024-03-11 09:28:33 +0900

    '레이아웃수정'

--- FaceMeet/src/components/Feed.js
+++ FaceMeet/src/components/Feed.js
@@ -3,8 +3,8 @@ import ProfileList from './ProfileList';
 import MeetingList from './MeetingList';
 import MeetingLogo from './MeetingLogo.png';
 import './Style.css';
+import { CiMicrophoneOff, CiVideoOff, CiSettings } from "react-icons/ci";
 
-// 해당 유저의 프로필 가져오는 코드 필요 **
 
 function Feed() {
     return (
@@ -13,7 +13,7 @@ function Feed() {
                 component="main"
                 sx={{
                     display: 'grid',
-                    maxHeight: '93vh',
+                    maxHeight: '100vh',
                     gridTemplateColumns: {
                         xs: 'repeat(6, 1fr)',
                         sm: 'repeat(6, 1fr)',
@@ -32,28 +32,78 @@ function Feed() {
                 {/* MeetingList Section */}
                 <Box component="section" sx={{ gridColumn: '1 / 2', gridRow: '2 / span 5', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
                     <MeetingList sx={{ height: '100%' }} />
+                    <Box sx={{
+                        position: 'relative',
+                        paddingTop: '10%',
+                        width: '16.3rem',
+                        height: '10rem',
+                    }} >
+                        <Box sx={{
+                            position: 'relative',
+                            backgroundColor: 'grey',
+                            marginLeft: '4%',
+                            marginTop: '1.5%',
+                            width: '3rem',
+                            height: '3rem',
+                            borderRadius: '15%',
+
+                        }} >
+
+                            <Box
+                                sx={{
+                                    position: 'absolute',
+                                    bottom: '0',
+                                    right: '0',
+                                    width: '17px',
+                                    height: '17px',
+                                    borderRadius: '50%',
+                                    backgroundColor: '#FF8A00', // 원 모양 도형의 배경색
+                                    border: '1px solid white', // 원 모양 도형의 테두리
+                                }}
+                            />
+                        </Box>
+                        <div style={{
+                            position: 'relative',
+                            marginLeft: '27%',
+                            marginTop: '-16%',
+                            fontWeight: 'bold'
+                        }}>홍길동</div>
+                        
+                        <div style = {{
+                            fontSize: '0.8rem',
+                            position: 'relative',
+                            marginLeft: '27%',
+                            marginTop: '0%',
+                            fontWeight: 'bold'
+                        }}> 온라인 </div>
+
+                            <div id='mic'><CiMicrophoneOff /> </div>
+
+                            <div id='video'> <CiVideoOff /> </div>
+
+                            <div id='setting'><CiSettings /> </div>
+                    </Box>
                 </Box>
 
                 {/* MeetingLogo Section (Top-left corner) */}
                 <Box component="section" sx={{ gridColumn: '1 / 2', gridRow: '1 / 2', display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start', marginLeft: '1rem', marginTop: '1rem' }}>
                     <img src={MeetingLogo} alt="Meeting 로고" style={{ width: '14rem', height: '8rem', marginLeft: '5%' }} />
                 </Box>
-                
-                <div style={{
-                    position: 'relative',
-                    backgroundColor: 'grey',
-                    marginLeft: '5px',
-                    width: '5rem',
-                    height: '5rem',
-                    borderRadius: '15%',
-                    marginTop: '20%',
-                    marginLeft: '30%',
 
-                }}>
-                </div>
-                <p style={{position: 'absolute', marginLeft: '21%', marginTop: '7.2%'}}>이름</p>
+
+                <p style={{ position: 'absolute', marginLeft: '21%', marginTop: '7.9%' }}>이름</p>
                 {/* ProfileList Section */}
-                <Box component="section" sx={{ gridColumn: '3 / 40', gridRow: '1 / span 1', display: 'flex', alignItems: 'center' }}>
+                <Box component="section" sx={{ gridColumn: '3 / 80 ', gridRow: '1 / span 1', display: 'flex', alignItems: 'center' }}>
+                    {/* 사용자의 프로필 - 데이터 받아서 이미지 뜨도록 하는 코드 필요*/}
+                    <Box sx={{
+                        position: 'relative',
+                        backgroundColor: 'grey',
+                        width: '5rem',
+                        height: '5rem',
+                        borderRadius: '15%',
+                        marginRight: '2%',
+
+                    }} />
                     <Box sx={{ flex: 1 }}>
                         <ProfileList />
                     </Box>

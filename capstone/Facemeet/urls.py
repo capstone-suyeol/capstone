638fa2552f122673437fbecbd277f3be6c9dac7e
KimMinGyun <kmg02845@gmail.com>
2024-04-02 16:08:04 +0900

    environment setting

--- capstone/Facemeet/urls.py
+++ capstone/Facemeet/urls.py
@@ -5,3 +5,19 @@ urlpatterns = [
     path('register/', views.register_user, name='register_user'),
     path('login/', views.login_user, name='login_user'),
 ]
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'users', views.CustomUserViewSet)
+router.register(r'meetings', views.MeetingViewSet)
+router.register(r'participants', views.ParticipantViewSet)
+router.register(r'friends', views.FriendViewSet)
+router.register(r'recordingfiles', views.RecordingFileViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

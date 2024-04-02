e54d3164de65aa5a16f842843e347613ce907a18
KimMinGyun <kmg02845@gmail.com>
2024-04-02 16:42:01 +0900

    video conference implement

---
+++ capstone/Facemeet/routing.py
@@ -0,0 +1,7 @@
+# your_app/routing.py
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/meeting/(?P<meeting_id>\w+)/$', consumers.MeetingConsumer.as_asgi()),
+]
--- capstone/capstone/asgi.py
+++ capstone/capstone/asgi.py
@@ -1,16 +1,17 @@
-"""
-ASGI config for capstone project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
+# asgi.py
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import Facemeet.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            your_app.routing.websocket_urlpatterns
+        )
+    ),
+})
